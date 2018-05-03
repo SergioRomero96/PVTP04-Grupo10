88177b0c3a070d96e7e7f6e6999afa57468ccb30
Sergio <Sergio@Sergio-PC>
2018-05-03 00:57:13 -0300

    agregado de la clase CalculadoraBean y CalculadoraFormBean

---
+++ src/java/aplicacion/punto3/beans/forms/CalculadoraFormBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.punto3.beans.forms;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Sergio Romero
+ */
+@ManagedBean
+@ViewScoped
+public class CalculadoraFormBean {
+
+    /**
+     * Creates a new instance of CalculadoraFormBean
+     */
+    public CalculadoraFormBean() {
+    }
+    
+}
